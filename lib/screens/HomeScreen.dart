6c1be5e06d95cad3342684b13d4a506250a4abd9
Adham Nour <adhamnourelwaffaa@gmail.com>
2021-08-20 22:40:26 +0200

    finished Home Screen

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -6,14 +6,59 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            child: Center(
-              child: Text("this is the dark home screen"),
-            ),
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('./assets/images/splash.png'),
+                    fit: BoxFit.cover)),
           ),
-          ElevatedButton(onPressed: () =>{}, child: Text('Next'))
+          Align(
+            child: Row(
+              children: [
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                        onPressed: () => print('MotroCycle'),
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.orange[700]),
+                        child: Text('Motrocycle')),
+                  ),
+                  flex: 1,
+                  fit: FlexFit.tight,
+                ),
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      onPressed: () => print('MotroCycle'),
+                      child: Text('Cars'),
+                      style:
+                          ElevatedButton.styleFrom(primary: Colors.orange[700]),
+                    ),
+                  ),
+                  flex: 1,
+                  fit: FlexFit.tight,
+                ),
+                Flexible(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            primary: Colors.orange[700]),
+                        onPressed: () => print('MotroCycle'),
+                        child: Text('Bicycles')),
+                  ),
+                  flex: 1,
+                  fit: FlexFit.tight,
+                ),
+              ],
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+            ),
+            alignment: Alignment(0, 0.5),
+          )
         ],
       ),
     );
